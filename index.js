8bc2d5a9619eb32eef254748b6270b524ec11e8c
gabo-hacStyle <65917023+gabo-hacStyle@users.noreply.github.com>
2022-02-03 18:54:54 -0500

    Fixing cors
    I wont use cors to restrict anyone, because isnt working properly

--- index.js
+++ index.js
@@ -10,17 +10,20 @@ const router_api = require('./api-routes/index')
 
 //Middlewear para ver información en formato JSON
 app.use(express.json());
+app.use(cors());
+      
+var whitelist = ['http://localhost:80', 'http://localhost:8000'];
 
-var whitelist = ['http://localhost:80', 'http://localhost:8000']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+//For now, I wont use cors to restrict anyone. 
+//var corsOptionsDelegate = function (req, callback) {
+//  var corsOptions;
+//  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+ //   corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+ // } else {
+ //   corsOptions = { origin: false } // disable CORS for this request
+ // }
+ // callback(null, corsOptions) // callback expects two parameters: error and options
+//}
 
 
 app.get('/', (req, res) => {
